d01f03360aa1ba39ec855d1aabc44539d2338afd
Dimitris112 <dhmhtrhsxios101@gmail.com>
2025-06-10 23:22:46 +0300

    Add solution 023

---
+++ 023/script.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function (obj) {
+    if (Array.isArray(obj)) {
+        return obj.map(compactObject).filter(Boolean);
+    }
+    if (obj && typeof obj === "object") {
+        return Object.fromEntries(
+            Object.entries(obj)
+                .map(([key, value]) => [key, compactObject(value)])
+                .filter(([_, value]) => Boolean(value))
+        );
+    }
+    return obj;
+};
