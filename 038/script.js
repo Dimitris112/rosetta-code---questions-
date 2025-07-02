4ddd130aa19d128bb64e8a2bc89babeaf01fac5e
Dimitris112 <dhmhtrhsxios101@gmail.com>
2025-07-02 21:03:25 +0300

    Add solution 038

---
+++ 038/script.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let insertPos = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos++] = nums[i];
+        }
+    }
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+};
