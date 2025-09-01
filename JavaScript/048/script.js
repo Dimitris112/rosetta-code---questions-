aa2604e8e1cea35b445884a29b684fa35cac5f24
Dimitris112 <dhmhtrhsxios101@gmail.com>
2025-09-01 12:57:09 +0300

    Add solution 048 JavaScript

---
+++ JavaScript/048/script.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+
+var findDifference = function (nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const result = [[], []]; // O(1)
+
+    set1.forEach(num => {
+        if (!set2.has(num)) {
+            result[0].push(num);
+        }
+    });
+
+    set2.forEach(num => {
+        if (!set1.has(num)) {
+            result[1].push(num);
+        }
+    });
+
+    return result;
+};
